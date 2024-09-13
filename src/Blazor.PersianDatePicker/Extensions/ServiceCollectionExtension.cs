20ef5eaa88100586501883ff3744361b6eb9f433
Farshad Davoudi <f.davoudi.r@outlook.com>
2024-09-13 23:47:22 +0330

    Add MarkHolidays

---
+++ src/Blazor.PersianDatePicker/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Blazor.PersianDatePicker.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddPersianDatePicker(this IServiceCollection services)
+    {
+
+    }
+}
